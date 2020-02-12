6c4beabe4e942e1e2b98e53f482060df8ffbcf44
Tysiachnyi <nikita.tysiachnyi@gmail.com>
2020-02-12 21:44:40 +0200

    done angular.io project

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   items = [];
-  constructor() { }
+
+  constructor(
+    private http: HttpClient
+  ) { }
 
   addToCart(product) {
     this.items.push(product);
@@ -18,6 +22,10 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+   getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
   }
 
 }
